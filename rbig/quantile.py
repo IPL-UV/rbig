Still leakage. Will fix later.

rbig/quantile.py
@@ -122,14 +122,11 @@ class QuantileTransformer(BaseEstimator, TransformerMixin):
     def _transform(self, X, inverse=False):
         for feature_idx in range(X.shape[1]):
             X[:, feature_idx] = self.transform_col(
-                X[:, feature_idx],
-                self.quantiles_[:, feature_idx],
-                self.references_,
-                inverse=inverse,
+                X[:, feature_idx], self.quantiles_[:, feature_idx], inverse=inverse
             )
         return X
 
-    def transform_col(self, X_col, quantiles, references, inverse):
+    def transform_col(self, X_col, quantiles, inverse):
 
         if not inverse:
             lower_bound_x = quantiles[0]
@@ -143,8 +140,8 @@ class QuantileTransformer(BaseEstimator, TransformerMixin):
             upper_bound_y = quantiles[1]
 
             # use CDF function
-            with np.errstate(invalid="ignore"):  # hide NAN comparison warnings
-                X_col = stats.norm.cdf(X_col)
+            # with np.errstate(invalid="ignore"):  # hide NAN comparison warnings
+            X_col = stats.norm.cdf(X_col)
 
         # Find index for lower and higher bounds
         with np.errstate(invalid="ignore"):
@@ -162,34 +159,44 @@ class QuantileTransformer(BaseEstimator, TransformerMixin):
             # If we don't do this, only one extreme of the duplicated is
             # used (the upper when we do ascending, and the
             # lower for descending). We take the mean of these two
+            # X_col[isfinite_mask] = 0.5 * (
+            #     interp1d(
+            #         quantiles, self.references_, kind="linear", fill_value="extrapolate"
+            #     )(X_col_finite)
+            #     - interp1d(
+            #         -quantiles[::-1],
+            #         -self.references_[::-1],
+            #         kind="linear",
+            #         fill_value="extrapolate",
+            #         copy=True,
+            #     )(-X_col_finite)
+            # )
             X_col[isfinite_mask] = 0.5 * (
-                interp1d(
-                    quantiles, references, kind="linear", fill_value="extrapolate"
-                )(X_col_finite)
-                - interp1d(
-                    -quantiles[::-1],
-                    -references[::-1],
-                    kind="linear",
-                    fill_value="extrapolate",
-                )(-X_col_finite)
+                np.interp(X_col_finite, quantiles, self.references_)
+                - np.interp(-X_col_finite, -quantiles[::-1], -self.references_[::-1])
             )
         else:
-            X_col[isfinite_mask] = interp1d(
-                references, quantiles, kind="linear", fill_value="extrapolate"
-            )(X_col_finite)
+            # X_col[isfinite_mask] = interp1d(
+            #     self.references_,
+            #     quantiles,
+            #     kind="linear",
+            #     fill_value="extrapolate",
+            #     copy=True,
+            # )(X_col_finite)
+            X_col[isfinite_mask] = np.interp(X_col_finite, self.references_, quantiles)
 
         X_col[upper_bounds_idx] = upper_bound_y
         X_col[lower_bounds_idx] = lower_bound_y
         # Forward - Match Output distribution
         if not inverse:
-            with np.errstate(invalid="ignore"):
-                X_col = stats.norm.ppf(X_col)
-                # find the value to clip the data to avoid mapping to
-                # infinity. Clip such that the inverse transform will be
-                # consistent
-                clip_min = stats.norm.ppf(BOUNDS_THRESHHOLD - np.spacing(1))
-                clip_max = stats.norm.ppf(1 - (BOUNDS_THRESHHOLD - np.spacing(1)))
-                X_col = np.clip(X_col, clip_min, clip_max)
+            # with np.errstate(invalid="ignore"):
+            X_col = stats.norm.ppf(X_col)
+            # find the value to clip the data to avoid mapping to
+            # infinity. Clip such that the inverse transform will be
+            # consistent
+            clip_min = stats.norm.ppf(BOUNDS_THRESHHOLD - np.spacing(1))
+            clip_max = stats.norm.ppf(1 - (BOUNDS_THRESHHOLD - np.spacing(1)))
+            X_col = np.clip(X_col, clip_min, clip_max)
             # else output distribution is uniform and the ppf is the
             # identity function so we let X_col unchanged
 