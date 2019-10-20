caadd17f6ecfc4705e9b2ff392e5694d7490d85d
Eman Johnson <emanjohnson91@gmail.com>
2019-10-20 15:10:03 +0200

    Added density mixin object with sampling method.

--- rbig/base.py
+++ rbig/base.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.utils import check_random_state
 
 
 class ScoreMixin(object):
@@ -19,3 +20,36 @@ class ScoreMixin(object):
             Mean log likelihood data points in X.
         """
         return np.mean(self.score_samples(X, y))
+
+
+class DensityMixin(object):
+    """Mixin for :func:`sample` that returns the """
+
+    def sample(self, n_samples=1, random_state=None):
+
+        # random state
+        rng = check_random_state(random_state)
+
+        # get features
+        d_dimensions = get_n_features(self)
+
+        # Gaussian samples
+        X_gauss = rng.randn(n_samples, d_dimensions)
+
+        # Inverse transformation
+        X = self.inverse_transform(X_gauss)
+        return X
+
+
+def get_n_features(model):
+
+    # Check if RBIG block
+    if hasattr(model, "d_dimensions"):
+        return model.d_dimensions
+
+    # Check if Model with RBIG block as attribute
+    elif hasattr(model.transforms[0], "d_dimensions"):
+        return model.transforms[0].d_dimensions
+
+    else:
+        raise ValueError("No model density (or block density) has been found.")
