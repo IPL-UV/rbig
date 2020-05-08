fixed jacobian error.
* working kde with epa kernel
* working marginal transformation
* need to reduce redundant functions
* need to do shape tests.

rbig/density/histogram.py
@@ -84,17 +84,17 @@ class ScipyHistogram(PDFEstimator):
         X_prob = self.estimator_.pdf(X.squeeze())
 
         X_prob = make_interior_probability(X_prob, eps=self.prob_tol)
-        return X_prob.reshape(-1, 1)
+        return X_prob
 
     def logpdf(self, X: np.ndarray) -> np.ndarray:
 
-        return np.log(self.pdf(X.squeeze())).reshape(-1, 1)
+        return np.log(self.pdf(X.squeeze()))
 
     def cdf(self, X: np.ndarray) -> np.ndarray:
-        return self.estimator_.cdf(X.squeeze()).reshape(-1, 1)
+        return self.estimator_.cdf(X.squeeze())
 
     def ppf(self, X: np.ndarray) -> np.ndarray:
-        return self.estimator_.ppf(X.squeeze()).reshape(-1, 1)
+        return self.estimator_.ppf(X.squeeze())
 
     def entropy(self, correction: bool = True) -> float:
         # calculate entropy
@@ -103,11 +103,7 @@ class ScipyHistogram(PDFEstimator):
         # MLE Estimator with Miller-Maddow Correction
         print(self.hist_counts_)
         if correction is True:
-            H += (
-                0.5
-                * (np.sum(self.hist_counts_ > 0) - 1)
-                / self.hist_counts_.sum()
-            )
+            H += 0.5 * (np.sum(self.hist_counts_ > 0) - 1) / self.hist_counts_.sum()
 
         return H
 
@@ -161,9 +157,7 @@ class QuantileHistogram(PDFEstimator):
         references = self.references_ * 100
 
         if self.subsample < X.shape[0]:
-            subsample_idx = rng.choice(
-                X.shape[0], size=self.subsample, replace=False
-            )
+            subsample_idx = rng.choice(X.shape[0], size=self.subsample, replace=False)
 
             X = X.take(subsample_idx, axis=0, mode="clip")
 
@@ -180,18 +174,14 @@ class QuantileHistogram(PDFEstimator):
 
     def logpdf(self, X: np.ndarray) -> np.ndarray:
 
-        return np.log(self.pdf(X))
+        return np.log(self.pdf(X.squeeze()))
 
     def cdf(self, X: np.ndarray) -> np.ndarray:
 
-        return np.interp(
-            X.squeeze(), self.quantiles_, self.references_
-        ).reshape(-1, 1)
+        return np.interp(X.squeeze(), self.quantiles_, self.references_)
 
     def ppf(self, X: np.ndarray) -> np.ndarray:
-        return np.interp(
-            X.squeeze(), self.references_, self.quantiles_,
-        ).reshape(-1, 1)
+        return np.interp(X.squeeze(), self.references_, self.quantiles_,)
 
     def entropy(self, correction: bool = True) -> float:
         raise NotImplementedError

rbig/density/kde.py
@@ -16,6 +16,7 @@ from rbig.utils import get_support_reference, get_domain_extension
 from scipy.interpolate import interp1d
 from rbig.density.utils import kde_cdf
 from rbig.density.empirical import estimate_empirical_cdf
+from KDEpy import FFTKDE
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -43,9 +44,7 @@ def exact_kde_est_pdf(
     # fit KDE Model
     if fft is False:
         estimator = stats.gaussian_kde(X, bw_method=bw_method,)
-        estimator.fit(
-            kernel="gau", bw_method=bw_method, fft=True, gridsize=nbins
-        )
+        estimator.fit(kernel="gau", bw_method=bw_method, fft=True, gridsize=nbins)
 
         hpdf = estimator.pdf(hbins)
     else:
@@ -72,20 +71,20 @@ class KDEScipy(PDFEstimator):
         estimator = stats.gaussian_kde(X.squeeze(), bw_method=self.bw_method,)
 
         self.hbins_ = get_support_reference(
-            support=X,
+            support=X.squeeze(),
             extension=self.support_extension,
             n_quantiles=self.n_quantiles,
         )
-        self.hpdf_ = np.exp(estimator.logpdf(self.hbins_.squeeze()))
 
-        self.hcdf_ = np.vectorize(
-            lambda x: estimator.integrate_box_1d(-np.inf, x)
-        )(self.hbins_.squeeze())
+        self.hcdf_ = np.vectorize(lambda x: estimator.integrate_box_1d(-np.inf, x))(
+            self.hbins_.squeeze()
+        )
+        self.hpdf_ = estimator.evaluate(self.hbins_.squeeze())
 
         return self
 
     def pdf(self, X: np.ndarray) -> np.ndarray:
-        return np.interp(X, self.hbins_, self.hpdf_)
+        return np.interp(X.squeeze(), self.hbins_, self.hpdf_)
 
     def logpdf(self, X: np.ndarray) -> np.ndarray:
 
@@ -117,7 +116,7 @@ class KDEFFT(KDEScipy, PDFEstimator):
     def fit(self, X: np.ndarray, y: Optional[np.ndarray] = None) -> None:
         # fit model
         self.hbins_ = get_support_reference(
-            support=X,
+            support=X.squeeze(),
             extension=self.support_extension,
             n_quantiles=self.n_quantiles,
         )
@@ -125,16 +124,62 @@ class KDEFFT(KDEScipy, PDFEstimator):
         estimator = sm.nonparametric.KDEUnivariate(X.squeeze())
 
         estimator.fit(
-            kernel="gau",
-            bw=self.bw_method,
-            fft=True,
-            gridsize=self.n_quantiles,
+            kernel="gau", bw=self.bw_method, fft=True, gridsize=self.n_quantiles,
         )
 
+        # evaluate cdf from KDE estimator
         self.hpdf_ = estimator.evaluate(self.hbins_.squeeze())
 
-        self.hcdf_ = estimate_empirical_cdf(X, self.hbins_)
+        # estimate the empirical CDF function from data
+        self.hcdf_ = estimate_empirical_cdf(X.squeeze(), self.hbins_)
+
         # self.hcdf_ = kde_cdf(X, self.hbins_, bw=estimator.bw)
+
+        return self
+
+
+class KDEEpanechnikov(KDEScipy, PDFEstimator):
+
+    """
+    Parameters
+    ----------
+    kernel : str, default='epa'
+        Many kernels available. For this toolbox, I recommend the
+        epa kernel for fast PDF evaluation.
+
+    bw_method : str, default='ISJ',
+    """
+
+    def __init__(
+        self,
+        kernel: "epa",
+        bw_method: Union[float, str] = "ISJ",
+        n_quantiles: int = 1_000,
+        support_extension: Union[int, float] = 10,
+        norm: int = 2,
+    ) -> None:
+        self.kernel = kernel
+        self.bw_method = bw_method
+        self.n_quantiles = n_quantiles
+        self.support_extension = support_extension
+        self.norm = norm
+
+    def fit(self, X: np.ndarray, y: Optional[np.ndarray] = None) -> None:
+        # fit model
+        # estimate the empirical CDF function from data
+        self.hbins_ = get_support_reference(
+            support=X.squeeze(),
+            extension=self.support_extension,
+            n_quantiles=self.n_quantiles,
+        )
+        estimator = FFTKDE(kernel=self.kernel, bw=self.bw_method, norm=self.norm)
+
+        estimator.fit(X.squeeze())
+
+        # evaluate cdf from KDE estimator
+        self.hpdf_ = estimator.evaluate(self.hbins_.squeeze())
+
+        self.hcdf_ = estimate_empirical_cdf(X.squeeze(), self.hbins_)
 
         return self
 
@@ -170,18 +215,11 @@ class KDESklearn(KDEScipy, PDFEstimator):
 
         # get reference support
         self.hbins_ = get_support_reference(
-            support=X,
-            extension=self.support_extension,
-            n_quantiles=self.n_quantiles,
+            support=X, extension=self.support_extension, n_quantiles=self.n_quantiles,
         )
         self.hpdf_ = np.exp(estimator.score_samples(self.hbins_[:, None]))
 
         # get bin widths
-        hbin_widths = self.hbins_[1:] - self.hbins_[:-1]
-
-        # get cdf
-        hcdf_ = np.cumsum(self.hpdf_[1:] * hbin_widths)
-
-        self.hcdf_ = np.hstack([0.0, hcdf_])
+        self.hcdf_ = estimate_empirical_cdf(X.squeeze(), self.hbins_)
 
         return self

rbig/transform/base.py
@@ -7,9 +7,7 @@ from rbig.density.base import PDFEstimator
 
 
 class BaseTransform(BaseEstimator, TransformerMixin):
-    def log_abs_det_jacobian(
-        self, X: np.ndarray, y: Optional[np.ndarray] = None
-    ) -> float:
+    def log_abs_det_jacobian(self, X: np.ndarray) -> float:
         raise NotImplementedError
 
 
@@ -19,7 +17,7 @@ class DensityMixin(object):
     @abstractmethod
     def score_samples(
         self, X: np.ndarray, y: Optional[np.ndarray] = None
-    ) -> float:
+    ) -> np.ndarray:
         """Return the mean log likelihood (or log(det(Jacobian))).
         Parameters
         ----------

rbig/transform/gaussianization.py
@@ -17,6 +17,125 @@ import warnings
 from sklearn.base import clone
 
 
+class Gaussianization(BaseTransform, DensityMixin):
+    """class to take a univariate Gaussianization
+    
+    This class composes a uniform transformer and a Inverse Gauss CDF
+    transformation to make a Gaussianization transformation
+
+    Parameters
+    ----------
+    uni_transformer : BaseTransform
+        any base transformation that transforms data to a 
+        uniform distribution.
+    
+    """
+
+    def __init__(self, uni_transformer) -> None:
+        self.uni_transformer = uni_transformer
+
+    def fit(self, X: np.ndarray) -> None:
+        """Fits the uniform transformation to the data
+        
+        Parameters
+        ----------
+        X : np.ndarray, (n_samples, n_features)
+            the input data to be transformed
+        
+        Returns
+        -------
+        self : instance of self
+        """
+        # check inputs
+        X = check_array(X, ensure_2d=False, copy=True)
+
+        # fit uniformization to data
+        self.uni_transformer.fit(X)
+
+        return self
+
+    def transform(self, X: np.ndarray) -> np.ndarray:
+        """Forward transformation of X.
+        
+        Parameters
+        ----------
+        X : np.ndarray, (n_samples, n_features)
+            the data to be transformed to the gaussian domain
+        
+        Returns
+        -------
+        Xtrans : np.ndarray, (n_samples, n_features)
+            the transformed Gaussianized data
+        """
+        # check inputs
+        X = check_array(X, ensure_2d=False, copy=True)
+
+        # transform data to uniform domain
+        X = self.uni_transformer.transform(X)
+
+        # transform data to gaussian domain
+        X = InverseGaussCDF().transform(X)
+
+        # return gaussianized variable
+        return X
+
+    def inverse_transform(self, X: np.ndarray) -> np.ndarray:
+        """Performs the inverse transformation to original domain
+        
+        This transforms univariate Gaussian data to the original
+        domain of the fitted transformation.
+
+        Parameters
+        ----------
+        X : np.ndarray, (n_samples, n_features)
+            Gaussian data
+        
+        Returns
+        -------
+        X : np.ndarray, (n_samples, n_features)
+            the data in the original data domain.
+        """
+        # check inputs
+        X = check_array(X, ensure_2d=False, copy=True)
+
+        # transform data to uniform domain
+        X = InverseGaussCDF().inverse_transform(X)
+
+        # transform data to origin domain
+        X = self.uni_transformer.inverse_transform(X)
+
+        # return data from original domain
+        return X
+
+    def log_abs_det_jacobian(self, X: np.ndarray) -> np.ndarray:
+        """Calculates the log-det-jacobian of the transformation
+        
+        Parameters
+        ----------
+        X : np.ndarray, (n_samples, n_features)
+            the data to be transformed
+        
+        Returns
+        -------
+        Xslogdet : np.ndarray, (n_samples, n_features)
+            the log det-jacobian for each sample
+        """
+        # check array
+        X = check_array(X.reshape(-1, 1), ensure_2d=True, copy=True)
+
+        # find uniform probability
+        u_log_prob = self.uni_transformer.log_abs_det_jacobian(X)
+
+        # transform data into gaussian domain
+        X_g = self.transform(X)
+
+        # find gaussian probability
+        g_log_prob = stats.norm().logpdf(X_g.squeeze())
+
+        # return combined log det-jacobian
+        return u_log_prob - g_log_prob
+
+
 class MarginalGaussianization(BaseTransform, DensityMixin):
     def __init__(self, uni_transformer) -> None:
         self.uni_transformer = uni_transformer
@@ -39,12 +158,9 @@ class MarginalGaussianization(BaseTransform, DensityMixin):
 
         for feature_idx in range(X.shape[1]):
 
-            # Marginal Uniformization
             X[:, feature_idx] = (
                 self.transforms_[feature_idx].transform(X[:, feature_idx]).squeeze()
             )
-            # Gaussian Quantile Function
-            X[:, feature_idx] = InverseGaussCDF().transform(X[:, feature_idx]).squeeze()
 
         return X
 
@@ -52,11 +168,6 @@ class MarginalGaussianization(BaseTransform, DensityMixin):
         X = check_array(X, ensure_2d=True, copy=True)
 
         for feature_idx in range(X.shape[1]):
-
-            # Gaussian CDF
-            X[:, feature_idx] = (
-                InverseGaussCDF().inverse_transform(X[:, feature_idx]).squeeze()
-            )
 
             X[:, feature_idx] = (
                 self.transforms_[feature_idx]
@@ -71,30 +182,13 @@ class MarginalGaussianization(BaseTransform, DensityMixin):
     ) -> float:
         X = check_array(X, ensure_2d=True, copy=True)
         for feature_idx in range(X.shape[1]):
-            # plt.figure()
-            # plt.hist(X[:, feature_idx])
-            u_prob = (
+
+            X[:, feature_idx] = (
                 self.transforms_[feature_idx]
                 .log_abs_det_jacobian(X[:, feature_idx])
                 .squeeze()
             )
-            g_prob = stats.norm().logpdf(
-                self.transforms_[feature_idx].transform(X[:, feature_idx],).squeeze()
-            )
-            X[:, feature_idx] = u_prob - g_prob
-            # plt.figure()
-            # plt.hist(temp)
-
         return X
-
-    def score_samples(
-        self, X: np.ndarray, y: Optional[np.ndarray] = None
-    ) -> np.ndarray:
-        X = check_array(X, ensure_2d=True, copy=False)
-
-        x_logprob = stats.norm().logpdf(self.transform(X))
-
-        return (x_logprob + self.log_abs_det_jacobian(X)).sum(axis=1)
 
 
 # class QuantileGaussianization(QuantileTransformer, DensityMixin):

rbig/transform/uniformization.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy.random import RandomState
 from scipy import stats
 from rbig.density.histogram import ScipyHistogram, QuantileHistogram
-from rbig.density.kde import KDEFFT, KDEScipy, KDESklearn
+from rbig.density.kde import KDEFFT, KDEScipy, KDESklearn, KDEEpanechnikov
 
 # Base classes
 from sklearn.utils import check_array, check_random_state
@@ -83,7 +83,7 @@ class HistogramUniformization(BaseTransform, DensityMixin):
         X : np.ndarray
             transformed data
         """
-        X = check_array(X.reshape(-1, 1), ensure_2d=True, copy=True)
+        X = check_array(X, ensure_2d=False, copy=True)
 
         return self.estimator_.cdf(X)
 
@@ -103,13 +103,11 @@ class HistogramUniformization(BaseTransform, DensityMixin):
         X : np.ndarray, (n_samples, 1)
             Transformed data
         """
-        X = check_array(X.reshape(-1, 1), ensure_2d=True, copy=True)
+        X = check_array(X, ensure_2d=False, copy=True)
 
         return self.estimator_.ppf(X)
 
-    def score_samples(
-        self, X: np.ndarray, y: Optional[np.ndarray] = None
-    ) -> float:
+    def score_samples(self, X: np.ndarray, y: Optional[np.ndarray] = None) -> float:
         """Returns the log likelihood. It
         calculates the mean of the log probability.
         """
@@ -136,7 +134,7 @@ class HistogramUniformization(BaseTransform, DensityMixin):
         X_jacobian : (n_samples, n_features),
             the mean of the log probability
         """
-        X = check_array(X.reshape(-1, 1), ensure_2d=True, copy=True)
+        X = check_array(X, ensure_2d=False, copy=True)
 
         return self.estimator_.logpdf(X)
 
@@ -179,6 +177,7 @@ class KDEUniformization(HistogramUniformization):
         algorithm: str = "kd_tree",
         kernel: str = "gaussian",
         metric: str = "euclidean",
+        norm: int = 2,
         kwargs: Dict = {},
     ) -> None:
         self.bw_method = bw_method
@@ -188,6 +187,7 @@ class KDEUniformization(HistogramUniformization):
         self.algorithm = algorithm
         self.metric = metric
         self.kernel = kernel
+        self.norm = norm
         self.kwargs = kwargs
 
     def fit(self, X: np.ndarray, y: Optional[np.ndarray] = None) -> None:
@@ -212,6 +212,14 @@ class KDEUniformization(HistogramUniformization):
                 bw_method=self.bw_method,
                 n_quantiles=self.n_quantiles,
                 support_extension=self.support_extension,
+            ).fit(X)
+        elif self.method in ["epa"]:
+            self.estimator_ = KDEEpanechnikov(
+                kernel=self.kernel,
+                bw_method=self.bw_method,
+                n_quantiles=self.n_quantiles,
+                support_extension=self.support_extension,
+                norm=self.norm,
             ).fit(X)
         else:
             raise ValueError(f"Unrecognized method: {self.method}")
@@ -241,9 +249,7 @@ class MarginalUniformization(BaseTransform, DensityMixin):
         for feature_idx in range(X.shape[1]):
 
             X[:, feature_idx] = (
-                self.transforms_[feature_idx]
-                .transform(X[:, feature_idx])
-                .squeeze()
+                self.transforms_[feature_idx].transform(X[:, feature_idx]).squeeze()
             )
 
         return X