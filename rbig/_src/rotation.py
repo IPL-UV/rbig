8b14c6c70edcb22d8624c6af153cd32709273296
Juan Emmanuel Johnson <juan.johnson@uv.es>
2021-11-19 16:56:42 +0100

    added an orthogonal ica rotation.

--- rbig/_src/rotation.py
+++ rbig/_src/rotation.py
@@ -1,6 +1,46 @@
 import numpy as np
+from picard import Picard
 from sklearn.decomposition import PCA
 from scipy.stats import ortho_group
+
+
+class ICARotation:
+    name: str = "ica"
+
+    def __init__(
+        self,
+        X: np.ndarray,
+        random_state=123,
+        max_iter=100,
+        **kwargs
+    ):
+
+        # create pca object
+        self.estimator = Picard(
+            ortho=True,
+            whiten=False,
+            extended=None,
+            random_state=random_state,
+            max_iter=max_iter,
+            **kwargs
+        ).fit(X)
+
+    def forward(self, X):
+
+        Z = self.estimator.transform(X)
+
+        return Z
+
+    def inverse(self, Z):
+        X = self.estimator.inverse_transform(Z)
+
+        return X
+
+    def gradient(self, X):
+
+        X_grad = np.zeros(X.shape[0])
+
+        return X_grad
 
 
 class PCARotation:
