092d78a0ea5f9670c5cd4f70ff054ec58ff309af
Juan Emmanuel Johnson <juan.johnson@uv.es>
2021-11-19 17:05:07 +0100

    added top level imports.

--- rbig/__init__.py
+++ rbig/__init__.py
@@ -1,10 +1,28 @@
 from ._src.base import FlowModel
+from ._src.model import RBIG
 from ._src.training import train_rbig_info_loss
 from ._src.uniform import MarginalHistogramUniformization, MarginalKDEUniformization
 from ._src.invcdf import InverseGaussCDF
-from ._src.rotation import RandomRotation, PCARotation
+from ._src.rotation import RandomRotation, PCARotation, ICARotation
 from ._src.entropy import entropy_marginal, entropy_rbig, entropy_univariate
 from ._src.losses import neg_entropy_normal, negative_log_likelihood
 from ._src.total_corr import information_reduction, rbig_total_corr
 
-__all__ = ["FlowModel"]
+__all__ = [
+    "FlowModel",
+    "RBIG",
+    "train_rbig_info_loss",
+    "MarginalHistogramUniformization",
+    "MarginalKDEUniformization",
+    "InverseGaussCDF",
+    "PCARotation",
+    "ICARotation",
+    "RandomRotation",
+    "entropy_marginal",
+    "entropy_rbig",
+    "entropy_univariate",
+    "neg_entropy_normal",
+    "negative_log_likelihood",
+    "information_reduction",
+    "rbig_total_corr",
+]
