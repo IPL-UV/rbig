68884e0ec49859b5d69e71690a80b0ac1d7517a4
mattclifford1 <matt.clifford@bristol.ac.uk>
2023-03-14 16:23:02 +0000

    requirements from .txt file for install_requires

--- setup.py
+++ setup.py
@@ -2,6 +2,17 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+   
+def dependencies_from_file(file_path):
+    required = []
+    with open(file_path) as f:
+        for l in f.readlines():
+            l_c = l.strip()
+            # get not empty lines and ones that do not start with python
+            # comment "#" (preceded by any number of white spaces)
+            if l_c and not l_c.startswith('#'):
+                required.append(l_c)
+    return required
 
 setuptools.setup(
     name="py_rbig",
@@ -13,6 +24,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jejjohnson/rbig",
     packages=setuptools.find_packages(),
+    install_requires=dependencies_from_file('requirements.txt'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
